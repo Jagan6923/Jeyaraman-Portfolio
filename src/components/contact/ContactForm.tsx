6390c971ba1a4cb103f46d65de99ee1feb2de55e
Jagan6923 <jeyaraman.saravanan23@gmail.com>
2025-04-10 20:25:31 +0530

    toast added

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -23,7 +23,31 @@ export default function ContactForm() {
         })
         .then(
           () => {
-            toast.success("Happy to get your message! I'll reply soon. 😊");
+            toast.success("Happy to get your message! I'll reply soon. 😊", {
+              icon: (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+              ),
+              style: {
+                background: "#07bc0c",
+                color: "#fff",
+                fontWeight: "600",
+                fontSize: "16px",
+              },
+            });
+
             setFormData({ name: "", email: "", message: "" });
           },
           (error) => {
